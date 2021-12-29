b215c4f3868e49da296a395918272f96f65ea4df
Kmin <82199372+dev-min2@users.noreply.github.com>
2021-12-29 14:50:00 +0900

    1229Echo,OvelappedIO

---
+++ Server/include/std.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <winsock2.h>
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <vector>
+#include <process.h>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#pragma comment(lib,"ws2_32.lib")
+#define SERVER_IP "127.0.0.1"
+#define PORT_NUM 9123
+#define BUF_SIZE 1024
